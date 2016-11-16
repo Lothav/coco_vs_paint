dfad8a28fd8e701ae4b14bddfa59fd65da2a7623
Luiz0tavio <luizotrv@gmail.com>
2016-11-16 21:33:47 -0200

    commit script

--- commit.sh
+++ commit.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 gulp &&
-git commit -am \'"$1"\' && git push origin master
-#cp coco_vs_paint.js ../LuizOtav.io/coco_vs_paint &&
-#cp -r assets ../LuizOtav.io/coco_vs_paint &&
-#rm coco_vs_paint &&
-#cd ../LuizOtav.io &&
-#git add * && git commit -m "changes in Luiz0tavio/coco_vs_paint" && git push origin master
+git commit -am "$1" && git push origin master &&
+mv coco_vs_paint.js ../LuizOtav.io/coco_vs_paint &&
+cp -r assets ../LuizOtav.io/coco_vs_paint &&
+cd ../LuizOtav.io &&
+git commit -am "changes in @Luiz0tavio/coco_vs_paint -> $1" && git push origin master
